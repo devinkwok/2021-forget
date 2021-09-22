bcac7bf40f9ea884d056eaff03d2694cd478d3ae
Devin Kwok <devin@devinkwok.com>
2021-09-21 22:49:34 -0400

    FIX bug ADD logging and timing.

--- forget/postprocess/weight_stats.py
+++ forget/postprocess/weight_stats.py
@@ -1,11 +1,13 @@
 import os
+import time
 import numpy as np
 from itertools import chain
 from matplotlib import pyplot as plt
 
-def PlotWeights():
+class PlotWeights():
 
     def __init__(self, job):
+        start_time = time.perf_counter()
         self.job = job
         self.layers = {}
         for epoch in range(job.n_epochs):
@@ -13,10 +15,13 @@ def PlotWeights():
             for rep, ckpt in enumerate(job.load_checkpoints(epoch, to_cpu=True)):
                 self.layers[epoch][rep] = {}
                 state_dict = ckpt['model_state_dict']
+                print(f'Loading model{rep} epoch={epoch}.pt with {len(state_dict)} params...')
                 for layer_name, layer in state_dict.items():
                     self.layers[epoch][rep][layer_name] = layer
+        print(f'Models loaded t={time.perf_counter - start_time}')
 
     def retrieve_layers(self, replicates=[], epochs=[], name_contains=[]):
+        start_time = time.perf_counter()
         if type(epochs) is int:
             epochs = [epochs]
         if type(replicates) is int:
@@ -26,8 +31,10 @@ def PlotWeights():
         if len(epochs) == 0:  # include all
             epochs = np.arange(self.job.n_epochs)
         if len(replicates) == 0:  # include all
-            epochs = np.arange(self.job.n_replicates)
-        
+            replicates = np.arange(self.job.n_replicates)
+
+        print(f'Filtering replicates={replicates}, epochs={epochs}, names={name_contains}...')
+        n_layers = 0
         retrieved_layers = {}
         for rep in replicates:
             for epoch in epochs:
@@ -37,9 +44,13 @@ def PlotWeights():
                         if layer_name not in retrieved_layers:
                             retrieved_layers[layer_name] = []
                             retrieved_layers[layer_name].append(layer)
+                            n_layers += 1
+        print(f'Retrieved {n_layers} layers t={time.perf_counter - start_time}')
         return retrieved_layers
 
     def plot_histograms(self, data, filename, n_bins=10):
+        print(f'Plotting {len(x_labels)}x{len(y_labels)} histograms...')
+        start_time = time.perf_counter()
         # data is list of outputs from retrieve_layers()
         # which is a list of dicts (layer names) of lists (layers)
         x_labels = sorted(data[0].keys())  # organize plots by sorted name
@@ -49,6 +60,7 @@ def PlotWeights():
             # flatten layers into single array
             data.append([np.flatten(np.stack(data[i][name]))
                         for i in y_labels])
+            print(f'Flattened {name} with {len(data[-1])} entries...')
             layer_min = min(layer_min, *[np.min(x) for x in data[-1]])
             layer_max = max(layer_max, *[np.max(x) for x in data[-1]])
         fig, axes = plt.subplots(len(x_labels), len(y_labels),
@@ -59,7 +71,9 @@ def PlotWeights():
                 axes[i].set_title(x_labels[i])
             if i % len(y_labels) == 0:
                 axes[i].set_ylabel(y_labels[len(data) // i])
-        plt.savefig(os.path.join(self.job.save_path, 'weight_histograms', filename))
+        fig_path = os.path.join(self.job.save_path, 'weight_histograms', filename)
+        print(f'Saving to {fig_path} t={time.perf_counter - start_time}.')
+        plt.savefig(fig_path)
 
     def hist_layers_by_init(self, name_contains=[]):
         last_epoch = self.job.n_epochs - 1  # use last epoch
