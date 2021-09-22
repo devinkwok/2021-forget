87ba0ca2dd1138d3323b7c55103b58933776604f
Devin Kwok <devin@devinkwok.com>
2021-09-21 23:31:37 -0400

    FIX bugs, detailed timing

--- forget/postprocess/weight_stats.py
+++ forget/postprocess/weight_stats.py
@@ -7,18 +7,19 @@ from matplotlib import pyplot as plt
 class PlotWeights():
 
     def __init__(self, job):
-        start_time = time.perf_counter()
+        print(f'Loading models...')
         self.job = job
         self.layers = {}
         for epoch in range(job.n_epochs):
             self.layers[epoch] = {}
             for rep, ckpt in enumerate(job.load_checkpoints(epoch, to_cpu=True)):
+                start_time = time.perf_counter()
                 self.layers[epoch][rep] = {}
                 state_dict = ckpt['model_state_dict']
-                print(f'Loading model{rep} epoch={epoch}.pt with {len(state_dict)} params...')
+                print(f'm={rep}, ep={epoch}, p={len(state_dict)}, t={time.perf_counter() - start_time}')
                 for layer_name, layer in state_dict.items():
                     self.layers[epoch][rep][layer_name] = layer
-        print(f'Models loaded t={time.perf_counter - start_time}')
+        print(f'Models loaded.')
 
     def retrieve_layers(self, replicates=[], epochs=[], name_contains=[]):
         start_time = time.perf_counter()
@@ -36,33 +37,35 @@ class PlotWeights():
         print(f'Filtering replicates={replicates}, epochs={epochs}, names={name_contains}...')
         n_layers = 0
         retrieved_layers = {}
-        for rep in replicates:
-            for epoch in epochs:
-                for layer_name, layer in self.layers[rep][epoch].items():
+        for epoch in epochs:
+            for rep in replicates:
+                for layer_name, layer in self.layers[epoch][rep].items():
                     if len(name_contains) == 0 or \
                             any(x in layer_name for x in name_contains):
                         if layer_name not in retrieved_layers:
                             retrieved_layers[layer_name] = []
                             retrieved_layers[layer_name].append(layer)
                             n_layers += 1
-        print(f'Retrieved {n_layers} layers t={time.perf_counter - start_time}')
+        print(f'Retrieved {n_layers} layers t={time.perf_counter() - start_time}')
         return retrieved_layers
 
-    def plot_histograms(self, data, filename, n_bins=10):
-        print(f'Plotting {len(x_labels)}x{len(y_labels)} histograms...')
-        start_time = time.perf_counter()
-        # data is list of outputs from retrieve_layers()
+    def plot_histograms(self, layer_dict, filename, n_bins=10):
+        # layer_dict is list of outputs from retrieve_layers()
         # which is a list of dicts (layer names) of lists (layers)
-        x_labels = sorted(data[0].keys())  # organize plots by sorted name
-        y_labels = [i for i in range(len(data))]
+        x_labels = sorted(layer_dict[0].keys())  # organize plots by sorted name
+        y_labels = [i for i in range(len(layer_dict))]
+        print(f'Plotting {len(x_labels)}x{len(y_labels)} histograms...')
+
         layer_min, layer_max = -1e9, 1e9
+        data = []
         for name in x_labels:
+            start_time = time.perf_counter()
             # flatten layers into single array
-            data.append([np.flatten(np.stack(data[i][name]))
-                        for i in y_labels])
-            print(f'Flattened {name} with {len(data[-1])} entries...')
+            data.append([np.stack(layer_dict[i][name]).flatten() for i in y_labels])
             layer_min = min(layer_min, *[np.min(x) for x in data[-1]])
             layer_max = max(layer_max, *[np.max(x) for x in data[-1]])
+            print(f'{name}, l={len(data[-1])}, t={time.perf_counter() - start_time}')
+
         fig, axes = plt.subplots(len(x_labels), len(y_labels),
                                 sharey=True, tight_layout=True)
         for i, x in enumerate(chain(*data)):
@@ -72,7 +75,7 @@ class PlotWeights():
             if i % len(y_labels) == 0:
                 axes[i].set_ylabel(y_labels[len(data) // i])
         fig_path = os.path.join(self.job.save_path, 'weight_histograms', filename)
-        print(f'Saving to {fig_path} t={time.perf_counter - start_time}.')
+        print(f'Saving to {fig_path}')
         plt.savefig(fig_path)
 
     def hist_layers_by_init(self, name_contains=[]):
